b55e8a4c8e9c79a05bbcbc1132b86be6e6bdfc9d
tzuhsun <hsungrass@gmail.com>
2025-06-19 15:33:30 +0200

    npm init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+console.log(`Visit http://localhost:${PORT} to view the app`);
